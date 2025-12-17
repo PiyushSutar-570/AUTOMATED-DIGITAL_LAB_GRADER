c8e00d08fe3bb2f3648788bb7f348634ed87850a
PiyushSutar-570 <piyushsutar570@gmail.com>
2025-12-17 18:33:24 +0530

    final deploy

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "http://localhost:5000" });
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : "http://localhost:5000/api",
+});
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
